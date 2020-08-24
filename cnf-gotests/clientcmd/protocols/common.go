4d2480cfd19af097e60d86fc855232c703f4ec86
Nikita Kononov <nkononov@redhat.com>
2020-08-24 11:47:23 +0300

    Added tcp unicast test support. Updated icmp tests.

--- cnf-gotests/clientcmd/protocols/common.go
+++ cnf-gotests/clientcmd/protocols/common.go
@@ -1,9 +1,25 @@
 package protocols
 
+import (
+	"fmt"
+	"log"
+	"os/exec"
+)
+
 // CommonTest keeps common vars from connectivity tests
 type CommonTest struct {
 	MTU             int
 	ServerIP        string
 	ProtocolVersion int
 	Negative        bool
+}
+
+//RunCommand runs command and return output
+func (ct *CommonTest) RunCommand(cmd string) (string, error) {
+	log.Printf("run command: %s", cmd)
+	commandOutput, err := exec.Command("/bin/sh", "-c", cmd).Output()
+	if err != nil {
+		return "", fmt.Errorf("command execution failed - %s due to the error - %s", cmd, err)
+	}
+	return string(commandOutput), nil
 }
