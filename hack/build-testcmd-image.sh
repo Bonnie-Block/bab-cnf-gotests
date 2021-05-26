abc25454a1dc4b043a3609076282fa6bd2480f4c
Evgeny Levin <elevin@redhat.com>
2021-05-26 14:31:17 +0300

    Added updating of the tests-image branch when creating the testcmd image.

--- hack/build-testcmd-image.sh
+++ hack/build-testcmd-image.sh
@@ -11,6 +11,7 @@ fi
 git remote update --prune
 CURRENT_BRANCH=$(git branch | grep \* | cut -d ' ' -f2)
 git checkout tests_image
+git pull  https://gitlab.cee.redhat.com/cnf/cnf-gotests.git tests_image
 if which docker; then
   docker build --no-cache -f cnf-gotests/Dockerfile -t cnf-gotests-client .
 else
