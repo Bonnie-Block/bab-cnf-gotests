6a84f232c2a03f4610fb099d6de4f965225f77cf
Steven Skeard <sskeard@redhat.com>
2022-11-03 18:53:44 +0000

    CNF-6496: Add test for cgu reporting missing policy

--- test/ran/talm/tests/talm_batching.go
+++ test/ran/talm/tests/talm_batching.go
@@ -73,56 +73,43 @@ var _ = Describe("Talm Batching Tests", func() {
 		// 		// https://issues.redhat.com/browse/CNF-6491
 		// 	})
 		// })
-		// Context("where a managed policy is missing", func() {
-		// 	It("reports the missing policy", func() {
-		// 		// Polarion test id 47955
-		// 		// https://issues.redhat.com/browse/CNF-6496
+		Context("where a managed policy is missing", func() {
+			// 47955
+			It("reports the missing policy", func() {
 
-		// 		// Initialize the cgu using a policy that doesn't exist
-		// 		// cgu := `
-		// 		// 	apiVersion: ran.openshift.io/v1alpha1
-		// 		// 	kind: ClusterGroupUpgrade
-		// 		// 	metadata:
-		// 		// 	name: talm-single-batch-test-policy-missing
-		// 		// 	namespace: default
-		// 		// 	spec:
-		// 		// 	clusterLabelSelectors:
-		// 		// 		- matchLabels:
-		// 		// 			vendor: OpenShift
-		// 		// 	managedPolicies:
-		// 		// 		- non-existent-policy
-		// 		// 	enable: true
-		// 		// `
+				By("create and enable a cgu with a managed policy that does not exist", func() {
+					err := rantalmhelper.CreateCguAndWait(
+						rantalmhelper.HubAPIClient,
+						[]string{
+							rantalmhelper.Spoke1Name,
+						},
+						[]string{
+							"non-existent-policy",
+						},
+						rantalmhelper.CguName,
+						rantalmhelper.Namespace,
+						1,
+						1,
+					)
+					Expect(err).ToNot(HaveOccurred())
+				})
 
-		// 		// Apply the cgu
-		// 		// rantalmhelper.ApplyTalmResource(cgu)
+				By("waiting for the cgu status to report the missing policy", func() {
+					// This should immediately error out so we don't need a long timeout
+					err := rantalmhelper.WaitForCguInCondition(
+						rantalmhelper.HubAPIClient,
+						rantalmhelper.CguName,
+						rantalmhelper.Namespace,
+						"Validated",
+						"Missing managed policies: [non-existent-policy] ",
+						"",
+						1*time.Minute,
+					)
+					Expect(err).ToNot(HaveOccurred())
+				})
 
-		// 		// Wait for the cgu condition to show the expected error message
-		// 		// err := wait.PollImmediate(
-		// 		// 	parameters.TalmTestPollInterval,
-		// 		// 	parameters.TalmDefaultReconcileTime,
-		// 		// 	func() (done bool, err error) {
-		// 		// 		// Get the progressing condition
-		// 		// 		condition, err := rantalmhelper.GetTalmCondition("talm-single-batch-test-policy-missing", "Validated")
-		// 		// 		// The condition may not exist when we call this, so we need to wait for it to exist
-		// 		// 		if err != nil {
-		// 		// 			return false, nil
-		// 		// 		}
-
-		// 		// 		// Check the error message
-		// 		// 		expectedError := "The ClusterGroupUpgrade CR has: missing managed policies: [non-existent-policy]"
-		// 		// 		if condition.Message == expectedError {
-		// 		// 			return true, nil
-		// 		// 		}
-		// 		// 		return false, nil
-		// 		// 	})
-		// 		// Expect(err).ToNot(HaveOccurred())
-
-		// 		// Update cgu to use a real policy
-
-		// 		// Wait for cgu to finish
-		// 	})
-		// })
+			})
+		})
 	})
 
 	Describe("Multiple batch test", Label("talmmultibatch"), func() {
