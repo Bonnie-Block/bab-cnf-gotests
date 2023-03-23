38a56bca02b092ff2fe89138e8710a901ba3d04c
elevin <elevin@redhat.com>
2023-03-23 13:55:04 -0400

    Skip bond ActiveActive tests

--- test/network/sriov/netsriovhelper/sriov_bond_helper.go
+++ test/network/sriov/netsriovhelper/sriov_bond_helper.go
@@ -1,3 +1,4 @@
+//nolint
 package netsriovhelper
 
 import (
@@ -110,6 +111,8 @@ func TestActiveActiveBondScenario(
 	mtu int,
 	sriovInfos *cluster.EnabledNodes,
 	protocol, bondMode, ipAddrServer, ipAddrClient string) {
+	// Remove the Skip once TCP issue with the switch is solved.
+	Skip("Skipped due to TCP issue with the switch")
 	By("Validating test parameters")
 
 	switchCredentials, err := nethelper.NewSwitchCredentials()
